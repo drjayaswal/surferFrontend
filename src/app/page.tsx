49e9fccd8182bb60c3bac176ef7964bb66dc17bc
Dhruv Ratan Jayaswal <dhruvratanjayaswal@Dhruvs-MacBook-Air.local>
2025-07-10 00:00:37 +0530

    add: ready to deploy 5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import { Suspense } from "react";
 import Home from "./content";
+import { Loader2 } from "lucide-react";
 
 export default function Page() {
   return (
     <Suspense
-      fallback={<div className="text-center p-8 text-lg">Loading...</div>}
+      fallback={
+        <div className="flex items-center justify-center h-screen">
+          <Loader2 className="animate-spin w-6 h-6 text-sky-500" />
+          <p className="ml-2 text-sm text-gray-600">Loading...</p>
+        </div>
+      }
     >
       <Home />
     </Suspense>
